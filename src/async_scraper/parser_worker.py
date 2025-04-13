ab6adf83abf52add7eeb3aacdcd4bd6f0da610a5
Tautvydas Belgeras <tautvydas.b@8devices.com>
2025-04-13 20:39:16 +0300

    Separate ParserWorker into multiple processes

--- src/async_scraper/parser_worker.py
+++ src/async_scraper/parser_worker.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from concurrent.futures import ProcessPoolExecutor
 
 from .common.models import ParserItemModel
 from .interfaces import ParserInterface
@@ -9,29 +10,38 @@ from .scrapers.books_to_scrape.parsers import BookModel
 logger = logging.getLogger(__name__)
 
 
+def parse(parser_queue: asyncio.Queue[ParserItemModel], database: JSONDatabase):
+    while True:
+        try:
+            item = parser_queue.get_nowait()
+        except (asyncio.QueueShutDown, asyncio.QueueEmpty):
+            logger.debug("ParserWorker shutting down")
+            break
+        url = item.url
+        parser_class = item.parser
+        contents = item.contents
+
+        logger.debug("Parsing:")
+        logger.debug(f"url: {url}")
+        logger.debug(f"parser: {parser_class}")
+
+        parser: ParserInterface = parser_class()
+        # TODO type needs to be enforced
+        parsed: BookModel = parser.parse(contents, url)
+        parser_queue.task_done()
+
+        asyncio.run(database.add(url, parsed.model_dump()))
+
+
 class ParserWorker:
     def __init__(self, parser_queue: asyncio.Queue[ParserItemModel], database: JSONDatabase):
         self.parser_queue = parser_queue
         self.database = database
 
     async def run(self):
-        while True:
-            try:
-                item = self.parser_queue.get_nowait()
-            except (asyncio.QueueShutDown, asyncio.QueueEmpty):
-                logger.debug("ParserWorker shutting down")
-                break
-            url = item.url
-            parser_class = item.parser
-            contents = item.contents
-
-            logger.debug("Parsing:")
-            logger.debug(f"url: {url}")
-            logger.debug(f"parser: {parser_class}")
-
-            parser: ParserInterface = parser_class()
-            # TODO type needs to be enforced
-            parsed: BookModel = parser.parse(contents, url)
-            self.parser_queue.task_done()
-
-            await self.database.add(url, parsed.model_dump())
+        logger.debug("Running parse processes")
+        with ProcessPoolExecutor() as executor:
+            # TODO make process amount configurable
+            for i in range(3):
+                executor.submit(parse, self.parser_queue, self.database)
+        logger.debug("Parse processes finished")
