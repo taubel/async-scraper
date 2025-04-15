64a86cf02b1c85c0a12a4b3b39cbf9fcab4fad17
Tautvydas Belgeras <tautvydas.b@8devices.com>
2025-04-15 19:21:53 +0300

    Update error handling in JSONDatabase

--- src/async_scraper/parser_database.py
+++ src/async_scraper/parser_database.py
@@ -1,8 +1,11 @@
 import json
+import logging
 import pathlib
 from typing import Any, Iterator
 
 from anyio import open_file
+
+logger = logging.getLogger(__name__)
 
 
 class JSONDatabase:
@@ -20,18 +23,27 @@ class JSONDatabase:
             data = json.loads(contents)
         except FileNotFoundError:
             data = {}
+        except json.JSONDecodeError:
+            logger.error("Failed to decode json in database file")
+            data = None
         return data
 
     async def add(self, key: str, value: Any):
         data = await self._read()
+        if data is None:
+            logger.error("Failed to read database")
+            return
 
         data[key] = value
         data_j = json.dumps(data)
         async with await open_file(self.path_to_file, "w") as f:
             await f.write(data_j)
 
-    async def get(self, key: str):
+    async def get(self, key: str) -> str | None:
         data = await self._read()
+        if data is None:
+            logger.error("Failed to read database")
+            return None
         return data[key]
 
     # TODO create separate implementations for a sync and async JSONDatabase
@@ -41,6 +53,9 @@ class JSONDatabase:
                 data = json.load(f)
         except FileNotFoundError:
             data = {}
+        except json.JSONDecodeError:
+            logger.error("Failed to decode json in database file")
+            return
 
         for key, value in data.items():
             yield key, value
